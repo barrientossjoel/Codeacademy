e871f2769cf22556168eb8a4920b5f7ee13e8bbe
Joel Barrientos <Barrientoss.joel@gmail.com>
2025-03-13 18:26:10 -0300

    Add player stats logging example with block scope variables

---
+++ Scope/4_Review_Scope/Practice.js
@@ -0,0 +1,21 @@
+const health = 100;
+const player = 'player';
+let level = 0;
+const logPlayerStats = () => {
+  let player = 'joel';
+  let level = 1;
+    if (level === 2){
+      let health = 100;
+      let armor = 10;
+      let attack = 2; 
+      console.log(health);
+    }
+    else if (level === 3){
+      let health = 150;
+      let armor = 12;
+      let attack = 3;
+    }
+     
+}
+logPlayerStats();
+console.log(level);
